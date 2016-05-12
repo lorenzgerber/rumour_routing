3d24aac936e3afe086edc9693fcf8bb97b909342
Johan Eklund <Johan Eklund>
2016-05-12 10:13:10 +0200

    Added_classtests

--- src/PositionTest.java
+++ src/PositionTest.java
@@ -1,8 +1,25 @@
+
+
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 /**
  * Created by loge on 11/05/16.
  */
+
 public class PositionTest
 {
+    @Test
+    public void testGetX()
+    {
+        Position pos = new Position(7,7);
+        assertTrue(pos.getX() == 7);
+    }
+
+    @Test
+    public void testGetY()
+    {
+        Position pos = new Position(7,7);
+        assertTrue(pos.getY() == 7);
+    }
 }
