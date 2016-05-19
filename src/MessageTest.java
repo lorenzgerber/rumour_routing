d5ec1443c416b7d7ab7e357adcc61382ef7ff50e
tommie lindberg <tommie lindberg>
2016-05-19 20:03:17 +0200

    Påbörjan av test för message

--- src/MessageTest.java
+++ src/MessageTest.java
@@ -1,8 +1,19 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 /**
  * Created by loge on 11/05/16.
  */
 public class MessageTest
 {
+    @Test
+    public void testTTL()
+    {
+        Message message = new Message(50,3);
+
+        if(message.reduceTTL() = 49);
+
+    }
+
 }
