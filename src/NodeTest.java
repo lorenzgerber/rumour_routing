9fe4156e225c37669c8f9bc901aaae576bab7e8b
Johan Eklund <Johan Eklund>
2016-05-22 18:37:29 +0200

    added_some_simple_tests

--- src/NodeTest.java
+++ src/NodeTest.java
@@ -1,8 +1,84 @@
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
-/**
- * Created by loge on 11/05/16.
- */
 public class NodeTest
 {
+    /**
+     * Test throws exception Nodes constructor returns Null
+     */
+
+    @Test
+    public void testNodeConstructorReturnsNotNull()
+    {
+        int nodeId = 7;
+        int currentTime = 0;
+        double probAgent = 0.5;
+        int ttlAgent = 50;
+        int numRecentSteps = 7;
+        Node node = new Node(nodeId, new Position(3, 5) , currentTime, probAgent, ttlAgent, numRecentSteps);
+        assertNotNull(node);
+    }
+
+    /**
+     * Test throws exception negative currentTime
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testThrowsExceptionNegativeCurrentTime()
+    {
+        int nodeId = 7;
+        int currentTime = -1;
+        double probAgent = 0.5;
+        int ttlAgent = 50;
+        int numRecentSteps = 7;
+        Node node = new Node(nodeId, new Position(3, 5) , currentTime, probAgent, ttlAgent, numRecentSteps);
+        assertNotNull(node);
+    }
+
+    /**
+     * Test throws exception negative probAgent
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testThrowsExceptionNegativeProbAgent()
+    {
+        int nodeId = 7;
+        int currentTime = 0;
+        double probAgent = -0.5;
+        int ttlAgent = 50;
+        int numRecentSteps = 7;
+        Node node = new Node(nodeId, new Position(3, 5) , currentTime, probAgent, ttlAgent, numRecentSteps);
+        assertNotNull(node);
+    }
+
+
+    /**
+     * Test throws exception negative ttlAgent
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testThrowsExceptionNegativeTtlAgent()
+    {
+        int nodeId = 7;
+        int currentTime = 0;
+        double probAgent = 0.5;
+        int ttlAgent = -50;
+        int numRecentSteps = 7;
+        Node node = new Node(nodeId, new Position(3, 5) , currentTime, probAgent, ttlAgent, numRecentSteps);
+        assertNotNull(node);
+    }
+
+    /**
+     * Test throws exception negative numRecentSteps
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testThrowsExceptionNegativeNumRecentSteps()
+    {
+        int nodeId = 7;
+        int currentTime = 0;
+        double probAgent = 0.5;
+        int ttlAgent = 50;
+        int numRecentSteps = -7;
+        Node node = new Node(nodeId, new Position(3, 5) , currentTime, probAgent, ttlAgent, numRecentSteps);
+        assertNotNull(node);
+    }
 }
+
