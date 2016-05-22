13f329ad8b464b16c85780eb7ab3a9de8a8ff59b
Johan Eklund <Johan Eklund>
2016-05-22 20:05:53 +0200

    added_some_simple_tests

--- src/QueryTest.java
+++ src/QueryTest.java
@@ -1,8 +1,70 @@
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
-/**
- * Created by loge on 11/05/16.
- */
 public class QueryTest
 {
+    /**
+     * Test that the constructor returns a non null object.
+     */
+    @Test
+    public void testConstructorOfRequest() {
+        int ttlQuery = 7;
+        int queryEventId = 7;
+        int numRecentNodes = 7;
+        assertNotNull(new Query(ttlQuery, queryEventId, new Node(7, new Position(3, 5), 7, 7, 7, 7), numRecentNodes));
+    }
+
+
+    /**
+     * Test that the constructor gives exception when given
+     * negative ttlQuery
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testConstructorGivesExceptionForNegativeTtlQuery()
+    {
+        int ttlQuery = -7;
+        int queryEventId = 7;
+        int numRecentNodes = 7;
+        new Query(ttlQuery, queryEventId, new Node(7, new Position(3, 5), 7, 7, 7, 7), numRecentNodes);
+    }
+
+    /**
+     * Test that the constructor gives exception when given
+     * negative queryEventId
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testConstructorGivesExceptionForNegativeQueryEventId()
+    {
+        int ttlQuery = 7;
+        int queryEventId = -7;
+        int numRecentNodes = 7;
+        new Query(ttlQuery, queryEventId, new Node(7, new Position(3, 5), 7, 7, 7, 7), numRecentNodes);
+    }
+
+    /**
+     * Test that the constructor gives exception when given
+     * negative numRecentNodes
+     */
+    @Test (expected = IllegalArgumentException.class)
+    public void testConstructorGivesExceptionForNegativeNumRecentNodes()
+    {
+        int ttlQuery = 7;
+        int queryEventId = 7;
+        int numRecentNodes = -7;
+        new Query(ttlQuery, queryEventId, new Node(7, new Position(3, 5), 7, 7, 7, 7), numRecentNodes);
+    }
+
+    /**
+     * Test to see if the enums are Null
+     */
+    @Test
+    public void testConstantsInEnumState() {
+        assertNotNull(Query.queryMode.SEARCH);
+        assertNotNull(Query.queryMode.TRACK);
+        assertNotNull(Query.queryMode.HOMING);
+    }
+
+    
 }
+
