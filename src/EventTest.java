2ca593f535ab61a7abc619dd988945d4e3b316da
Johan Eklund <kv03jed@cs.umu.se>
2016-05-23 12:27:50 +0200

    Comments

--- src/EventTest.java
+++ src/EventTest.java
@@ -3,13 +3,24 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * EventTest
+ *
+ * Tests event
+ */
 public class EventTest
 {
+    /**
+     * Checks if the constructor is Null
+     */
     @Test
     public void testConstructor() {
         assertNotNull(new Event(7, 7));
     }
 
+    /**
+     * Checks if increaseTime is able to make correct increments
+     */
     @Test
     public void testIncreaseTime(){
         int time = 12;
